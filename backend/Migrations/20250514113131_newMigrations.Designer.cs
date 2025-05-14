e933c4a191a0d76c0bd91a7f43e83d55a02f86d6
lordgrimx <sabridoga2000@gmail.com>
2025-05-14 14:31:48 +0300

    K6Test modeline ortam ve kullanıcı bilgilerini saklamak için yeni alanlar eklendi: EnvironmentId (integer) ve UserId (text).

--- backend/Migrations/20250514113131_newMigrations.Designer.cs
+++ backend/Migrations/20250514113131_newMigrations.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using WebTestUI.Backend.Data;
@@ -13,9 +14,11 @@ using WebTestUI.Backend.Data.Entities;
 namespace WebTestUI.Backend.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250514113131_newMigrations")]
+    partial class newMigrations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/Migrations/20250514113131_newMigrations.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebTestUI.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class newMigrations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EnvironmentId",
+                table: "k6_tests",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "k6_tests",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EnvironmentId",
+                table: "k6_tests");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "k6_tests");
+        }
+    }
+}
